61023c62e40e24ce70d4897bc2f1a3c1ee597081
Dev <devp2101@gmail.com>
2022-01-27 14:09:36 +0530

    sidebar unwanted code is deleted

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -9,6 +9,7 @@
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
                     ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+                    ['label' => 'User', 'icon' => 'user', 'url' => ['/user']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
                 ],
             ]
