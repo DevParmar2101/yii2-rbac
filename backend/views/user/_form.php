d3ba5d7303168c23eaa030ac801c783667a8780e
Dev <devp2101@gmail.com>
2022-01-27 15:14:48 +0530

    _form.php design and new attributes are added in this file

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -6,17 +6,43 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 /* @var $form yii\widgets\ActiveForm */
+
 ?>
 
 <div class="user-form box box-primary">
     <?php $form = ActiveForm::begin(); ?>
+
     <div class="box-body table-responsive">
+        <div class="row-lg">
 
-        <?= $form->field($model, 'status')->textInput() ?>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <?= $form->field($model,'username')->textInput(['class' => 'form-control w-100']); ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <?= $form->field($model,'email')->textInput(['class' => 'form-control w-100']); ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <?= $form->field($model,'role')->dropDownList($model->getList()); ?>
+            </div>
+
+        </div>
+
+        <div class="row-lg">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <?= $form->field($model,'password')->passwordInput(['class' => 'form-control w-100']); ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <?= $form->field($model,'confirm_password')->passwordInput(['class' => 'form-control w-100']); ?>
+            </div>
+        </div>
 
     </div>
+
     <div class="box-footer">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success btn-flat']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-danger']) ?>
+        <?= Html::a(Yii::t('app', 'Back'),['index'], ['class' => 'btn btn-warning']) ?>
+
     </div>
+
     <?php ActiveForm::end(); ?>
 </div>
