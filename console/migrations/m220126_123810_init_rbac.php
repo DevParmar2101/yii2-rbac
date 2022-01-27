a531c97da702759cb9adf0b8bff7159bda1dc06f
Dev <devp2101@gmail.com>
2022-01-27 12:56:49 +0530

    img, css and js folder moved to admin folder

--- console/migrations/m220126_123810_init_rbac.php
+++ console/migrations/m220126_123810_init_rbac.php
@@ -25,11 +25,11 @@ class m220126_123810_init_rbac extends Migration
         $auth->add($updatePost);
 
         // add "author" role and give this role the "createPost" permission
-        $author = $auth->createRole('author');
-        $auth->add($author);
+        $user = $auth->createRole('user');
+        $auth->add($user);
         // To define a permission of create post for the author role the below code is used
 
-//        $auth->addChild($author, $createPost);
+//        $auth->addChild($user, $createPost);
 
         // add "admin" role and give this role the "updatePost" permission
         // as well as the permissions of the "author" role
@@ -37,14 +37,29 @@ class m220126_123810_init_rbac extends Migration
         $auth->add($admin);
         // To define a permission of update post and the permission of author the below commited code is used
 
+        $time = time();
+        $this->batchInsert(
+            'user',
+            ['username', 'auth_key', 'verification_token', 'password_hash', 'password_reset_token', 'email', 'status', 'created_at', 'updated_at'],
+            array(
+                array('admin', 'O-oDBLVlj7obhJzovnP15-Soz9iUB', '', '$2y$13$.aRQImtxqZur2mQb0jivKugRBwZnvfu1GP2v1fNbC7P/EjArPLnl.', NULL, 'admin@gmail.com', '10', $time, $time),
+                array('user', 'O-oDBLVlj7obhJzovnP15-Soz', '', '$2y$13$.aRQImtxqZur2mQb0jivKugRBwZnvfu1GP2v1fNbC7P/EjArPLnl.', NULL, 'ticketer@gmail.com', '10', $time, $time),
+
+            )
+        );
+
+        $admin_id = \common\models\User::findOne(['email'=>'admin@gmail.com']);
+        $auth->assign($admin,1);
+        $user_id = \common\models\User::findOne(['email'=>'user@gmail.com']);
+        $auth->assign($user,2);
 //        $auth->addChild($admin, $updatePost);
-//        $auth->addChild($admin, $author);
+//        $auth->addChild($admin, $user);
 
         // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
         // usually implemented in your User model.
 
         // The Below code is used to assign users
-//        $auth->assign($author, 2);
+//        $auth->assign($user, 2);
 //        $auth->assign($admin, 1);
     }
 
