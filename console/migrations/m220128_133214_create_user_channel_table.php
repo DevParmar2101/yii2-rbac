cd652ff0c684f6ce60123112ccc29cc4577f44f4
Dev <devp2101@gmail.com>
2022-01-28 19:05:15 +0530

    migration code is added in create_user_channel_table

--- console/migrations/m220128_133214_create_user_channel_table.php
+++ console/migrations/m220128_133214_create_user_channel_table.php
@@ -12,9 +12,18 @@ class m220128_133214_create_user_channel_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%user_channel}}', [
-            'id' => $this->primaryKey(),
-        ]);
+        $query =<<<EOF
+CREATE TABLE `user_channel` (
+ `channel_id` int(11) NOT NULL AUTO_INCREMENT,
+ `user_id` int(11) NOT NULL,
+ `channel_name` varchar(225) NOT NULL,
+ `channel_category` smallint(4) NOT NULL,
+ `channel_sub_category` smallint(4) NOT NULL,
+ `channel_bio` text NOT NULL,
+ PRIMARY KEY (`channel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+EOF;
+        $this->execute($query);
     }
 
     /**
