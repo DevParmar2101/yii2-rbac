90ad5a9b4ad0c396c6bcc0aea4de0bf2a392a382
Dev <devp2101@gmail.com>
2022-01-28 22:31:12 +0530

    bug fixes

--- backend/views/user-channel/index.php
+++ backend/views/user-channel/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel backend\models\UserChannelSearch */
+/* @var $searchModel common\models\UserChannelSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'User Channels';
