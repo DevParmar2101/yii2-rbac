abd7f9757d73399c0baafa0334e4b6ab7a93e52f
Dev <devp2101@gmail.com>
2022-01-29 00:42:51 +0530

    new code is added in this SiteController.php file

--- frontend/views/site/channel.php
+++ frontend/views/site/channel.php
@@ -14,11 +14,38 @@ use yii\bootstrap4\Html;
 <div class="site-channel">
     <h3><?= Html::encode($this->title) ?></h3>
 
-    <?php $form = ActiveForm::begin(['id' => 'use-channel']) ?>
+    <?php $form = ActiveForm::begin(['id' => 'user-channel']) ?>
 
     <div class="row">
-        <div class="col-lg-6">
-            <?= $form->field($model,'channel')?>
+        <div class="col-lg-12">
+            <?= $form->field($model,'channel_name')->textInput()?>
         </div>
     </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <?= $form->field($model,'channel_category')->textInput()?>
+        </div>
+        <div class="col-lg-6">
+            <?= $form->field($model,'channel_sub_category')->textInput() ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <?= $form->field($model,'channel_bio')->textarea(['rows' => 10]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <?= $form->field($model,'status')->dropdownList($model->status())?>
+        </div>
+        <div class="col-lg-6">
+            <?= $form->field($model,'channel_profile')->fileInput() ?>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
 </div>
