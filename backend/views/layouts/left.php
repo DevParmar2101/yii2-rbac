2eb2d1eb115444ffaf73970edf63970c39b3fe9c
Dev <devp2101@gmail.com>
2022-01-28 18:34:48 +0530

    model path change in this file

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -10,6 +10,7 @@
                 'items' => [
                     ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
                     ['label' => 'User', 'icon' => 'user', 'url' => ['/user']],
+                    ['label' => 'Contact Form', 'icon' => 'handshake-o', 'url' => ['/contact-form']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
                 ],
             ]
