aa0b67648957758b5815047a23a0486a9ac2254a
Dev <devp2101@gmail.com>
2022-01-28 18:49:12 +0530

    model path change in this file

--- common/models/ContactForm.php
+++ common/models/ContactForm.php
@@ -15,6 +15,8 @@ use Yii;
  */
 class ContactForm extends \yii\db\ActiveRecord
 {
+    public $verifyCode;
+
     /**
      * {@inheritdoc}
      */
@@ -47,5 +49,23 @@ class ContactForm extends \yii\db\ActiveRecord
             'subject' => 'Subject',
             'body' => 'Body',
         ];
+    }
+
+
+    /**
+     * Sends an email to the specified email address using the information collected by this model.
+     *
+     * @param string $email the target email address
+     * @return bool whether the email was sent
+     */
+    public function sendEmail($email)
+    {
+        return Yii::$app->mailer->compose()
+            ->setTo($email)
+            ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
+            ->setReplyTo([$this->email => $this->name])
+            ->setSubject($this->subject)
+            ->setTextBody($this->body)
+            ->send();
     }
 }
