dd17c1c43607a6059d2ac2222a58ef2ad62b6984
Dev <devp2101@gmail.com>
2022-01-29 00:08:04 +0530

    alter_user_channel migration file is generated in this project

---
+++ console/migrations/m220128_183634_alter_user_channel.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220128_183634_alter_user_channel
+ */
+class m220128_183634_alter_user_channel extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220128_183634_alter_user_channel cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220128_183634_alter_user_channel cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
