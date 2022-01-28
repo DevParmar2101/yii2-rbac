5aa3459378eb341f202841d300b65db6f77b4ab1
Dev <devp2101@gmail.com>
2022-01-28 17:40:46 +0530

    contact folder is added in backend view folder

---
+++ backend/views/contact-form/_form.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\ContactForm */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="contact-form-form box box-primary">
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="box-body table-responsive">
+
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'subject')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'body')->textInput(['maxlength' => true]) ?>
+
+    </div>
+    <div class="box-footer">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success btn-flat']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
---
+++ backend/views/contact-form/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\ContactFormSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Contact Forms';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="contact-form-index box box-primary">
+    <div class="box-header with-border">
+        <?= Html::a('Create Contact Form', ['create'], ['class' => 'btn btn-success btn-flat']) ?>
+    </div>
+    <div class="box-body table-responsive no-padding">
+        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'layout' => "{items}\n{summary}\n{pager}",
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'id',
+                'name',
+                'email:email',
+                'subject',
+                'body',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+    </div>
+</div>
