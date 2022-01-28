bc0e69070e497d118b39368b6ec4948aef2b9c2e
Dev <devp2101@gmail.com>
2022-01-28 22:31:08 +0530

    bug fixes

--- backend/controllers/UserChannelController.php
+++ backend/controllers/UserChannelController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 
 use Yii;
 use common\models\UserChannel;
-use backend\models\UserChannelSearch;
+use common\models\UserChannelSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
