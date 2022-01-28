e9356a14282e274f428f93fa14541a6ec3887441
Dev <devp2101@gmail.com>
2022-01-28 17:41:09 +0530

    contact form model is added in common/models

---
+++ common/models/ContactForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contact_form".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $subject
+ * @property string $body
+ */
+class ContactForm extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contact_form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'subject', 'body'], 'required'],
+            [['name', 'email', 'subject'], 'string', 'max' => 125],
+            [['body'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'subject' => 'Subject',
+            'body' => 'Body',
+        ];
+    }
+}
