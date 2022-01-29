806b369665a7c6d5963b1caaa88d3bf24c8eaadf
Dev <devp2101@gmail.com>
2022-01-29 22:41:44 +0530

    code is added in create_category_table

--- console/migrations/m220129_170051_create_category_table.php
+++ console/migrations/m220129_170051_create_category_table.php
@@ -12,9 +12,16 @@ class m220129_170051_create_category_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%category}}', [
-            'id' => $this->primaryKey(),
-        ]);
+        $query =<<<EOF
+CREATE TABLE `category` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `category` varchar(255) NOT NULL,
+ `status` tinyint(2) NOT NULL,
+ `crreated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+EOF;
+        $this->execute($query);
     }
 
     /**
