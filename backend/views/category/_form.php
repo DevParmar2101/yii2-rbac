7969786d29fd4672852b6d5683ad0e784fa6dbd3
Dev <devp2101@gmail.com>
2022-01-31 10:45:43 +0530

    In backend/views folder category new view folder is added in this folder

---
+++ backend/views/category/_form.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Category */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="category-form box box-primary">
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="box-body table-responsive">
+
+        <?= $form->field($model, 'category')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'status')->textInput() ?>
+
+        <?= $form->field($model, 'crreated_at')->textInput() ?>
+
+    </div>
+    <div class="box-footer">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success btn-flat']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
