6219fccb1aeff43922c47843d487a0ab32f54ea9
Dev <devp2101@gmail.com>
2022-02-03 12:16:14 +0530

    ew code and some bugs also fixed

--- backend/views/sub-category/_form.php
+++ backend/views/sub-category/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
     <div class="box-body table-responsive">
 
-        <?= $form->field($model, 'category_id')->textInput() ?>
+        <?= $form->field($model, 'category_id')->dropDownList($model->category(),['prompt' => 'Select Category']) ?>
         <?= $form->field($model, 'sub_category')->textInput() ?>
 
     </div>
--- backend/views/sub-category/view.php
+++ backend/views/sub-category/view.php
@@ -25,8 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= DetailView::widget([
             'model' => $model,
             'attributes' => [
-                'id',
-                'category_id',
+                'sub_category'
             ],
         ]) ?>
     </div>
