6561cd319ec1ef7ea9319ce5df8f600154c1c981
Dev <devp2101@gmail.com>
2022-02-03 12:15:21 +0530

    New function is added in this model file

--- common/models/SubCategory.php
+++ common/models/SubCategory.php
@@ -3,6 +3,7 @@
 namespace common\models;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "sub_category".
@@ -45,6 +46,6 @@ class SubCategory extends \yii\db\ActiveRecord
         ];
     }
     public function category(){
-
+        return ArrayHelper::map(Category::find()->where(['status' => Category::ACTIVE])->all(),'id','category');
     }
 }
