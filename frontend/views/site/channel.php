3a662d72edba0bfecba2bde18aacd5411ded0edf
Dev <devp2101@gmail.com>
2022-02-04 18:22:19 +0530

    some new bugs are fixed in frontend/views/channel folder

--- frontend/views/site/channel.php
+++ frontend/views/site/channel.php
@@ -48,12 +48,40 @@ use yii\bootstrap4\Html;
             <?= $form->field($model,'channel_bio')->textarea(['rows' => 10]) ?>
         </div>
     </div>
+
     <div class="row">
         <div class="col-lg-6">
-            <?= $form->field($model,'status')->dropdownList($model->status())?>
+            <?= $form->field($model,'channel_banner')->widget(\kartik\file\FileInput::class,[
+
+                'pluginOptions' => [
+                    'initialPreviewAsData' => true,
+                    'initialPreview' => Yii::getAlias('@web/uploads/channel-banner/'.$model->channel_banner),
+                    'showPreview' =>true,
+                    'showCaption' => false,
+                    'showRemove' => false,
+                    'showCancel' => false,
+                    'showUpload' => false,
+                ],
+            ])?>
         </div>
         <div class="col-lg-6">
-            <?= $form->field($model,'channel_profile')->fileInput() ?>
+            <?= $form->field($model,'channel_profile')->widget(\kartik\file\FileInput::class,[
+
+                'pluginOptions' => [
+                    'initialPreviewAsData'=>true,
+                    'initialPreview' => Yii::getAlias('@web/uploads/channel-profile/'.$model->channel_profile),
+                    'showPreview' => true,
+                    'showCaption' => false,
+                    'showRemove' => false,
+                    'showCancel' => false,
+                    'showUpload' => false,
+                ],
+            ]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <?= $form->field($model,'status')->dropdownList($model->status())?>
         </div>
     </div>
 
